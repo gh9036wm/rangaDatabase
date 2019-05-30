d87a6fc9746e720ac4c2d53adab5ae9da3320408
JamesTran <james.tran2@target.com>
2019-05-29 20:07:11 -0500

    added database-demo

---
+++ database-demo/src/main/java/com/in28minutes/database/databasedemo/jpa/PersonJpaRepository.java
@@ -0,0 +1,39 @@
+package com.in28minutes.database.databasedemo.jpa;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public class PersonJpaRepository {
+
+    //connect to the database
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Person findById(int id) {
+        return entityManager.find(Person.class, id);//JPA
+    }
+
+    public Person update(Person person){
+        return entityManager.merge(person);
+    }
+
+    public Person insert(Person person){
+        return entityManager.merge(person);
+    }
+
+    public void deleteById(int id) {
+        Person person = findById(id);
+        entityManager.remove(person);
+    }
+
+
+
+
+
+}
